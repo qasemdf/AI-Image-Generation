b453139f21be3d81cb0d8a7fe8134ad179997745
qasemdf <qasemdf@miamioh.edu>
2024-05-28 21:14:38 -0400

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,6 @@
+import download from "./download.png";
+import logo from "./logo.svg";
+import preview from "./preview.png";
+import deanAI from "./DeanAIS.png";
+
+export { download, logo, preview, deanAI };
